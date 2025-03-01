3b3681ddeb7b7b965f58efba42b937e5aab79a60
prahallad <prahalladp1325@gmail.com>
2025-03-01 10:37:31 +0530

    added a red cube

--- game/src/Game.jsx
+++ game/src/Game.jsx
@@ -1,9 +1,13 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas } from '@react-three/fiber';
 
 function Game() {
     return (
         <Canvas>
             <ambientLight intensity={0.5} />
+            <mesh>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial color="red" />
+            </mesh>
         </Canvas>
     );
 }
